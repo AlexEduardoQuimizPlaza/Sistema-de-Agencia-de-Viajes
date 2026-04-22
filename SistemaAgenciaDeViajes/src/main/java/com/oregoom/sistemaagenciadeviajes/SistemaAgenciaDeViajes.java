659ad1cde2aaf4378823d4745799e4fe3e13def9
AlexEduardoQuimizPlaza <alex.quimizpla@ug.edu.ec>
2026-04-22 13:46:18 -0500

    prueba

--- SistemaAgenciaDeViajes/src/main/java/com/oregoom/sistemaagenciadeviajes/SistemaAgenciaDeViajes.java
+++ SistemaAgenciaDeViajes/src/main/java/com/oregoom/sistemaagenciadeviajes/SistemaAgenciaDeViajes.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.oregoom.sistemaagenciadeviajes;
-
-/**
- *
- * @author Aleiyv
- */
 public class SistemaAgenciaDeViajes {
 
     public static void main(String[] args) {
